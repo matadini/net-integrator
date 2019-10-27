d12280dcd907f03d7f7609d39aee1103b3d89629
Mateusz Iwanek <lord.matadini@gmail.com>
2019-10-27 11:37:40 +0100

    dodawanie mappingow jako pane

--- net-integrator-desktop/src/main/java/pl/inzynier/netintegrator/desktop/gui/pane/mapping/UrlMappingPane.java
+++ net-integrator-desktop/src/main/java/pl/inzynier/netintegrator/desktop/gui/pane/mapping/UrlMappingPane.java
@@ -11,18 +11,89 @@ import lombok.RequiredArgsConstructor;
 import pl.inzynier.netintegrator.client.mapping.UrlMappingClient;
 import pl.inzynier.netintegrator.client.mapping.dto.UrlMappingClientException;
 import pl.inzynier.netintegrator.client.mapping.dto.UrlMappingReadDto;
+import pl.inzynier.netintegrator.desktop.shared.JavaFxUtil;
 import pl.inzynier.netintegrator.http.util.RequestMethod;
 
+import java.net.URL;
 import java.util.List;
 
 @RequiredArgsConstructor
 public class UrlMappingPane extends BorderPane {
 
     @FXML
-    private Button buttonAdd;
+    private TabPane tabPane;
+
+    private final UrlMappingClient managmentClient;
+
+    private UrlMappingPaneAdd mappingController;
+    private UrlMappingPaneEdit mappingControllerEdit;
 
     @FXML
-    private Button buttonRemove;
+    private void initialize() {
+
+
+        try {
+
+            // dodawanie
+            mappingController = new UrlMappingPaneAdd(managmentClient);
+            mappingController = JavaFxUtil.loadFxml(mappingController, UrlMappingPaneAdd.class.getResource("UrlMappingPaneAdd.fxml"));
+
+
+            Tab tabAdd = createNoClosableTab("       Add       ");
+            tabAdd.setContent(mappingController);
+
+            // edycja i usuwanie
+            mappingControllerEdit = new UrlMappingPaneEdit(managmentClient);
+            mappingControllerEdit = JavaFxUtil.loadFxml(mappingControllerEdit, UrlMappingPaneEdit.class.getResource("UrlMappingPaneEdit.fxml"));
+
+            Tab tabEdit = createNoClosableTab(" Edit and remove ");
+            tabEdit.setContent(mappingControllerEdit);
+
+            // dodaj tabsy do widoku
+            ObservableList<Tab> tabs = tabPane.getTabs();
+            tabs.add(tabAdd);
+            tabs.add(tabEdit);
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+//
+//        try {
+//            ObservableList<RequestMethod> httpMethods = FXCollections.observableArrayList(RequestMethod.values());
+//            comboboxTargetMethod.setItems(httpMethods);
+//            comboboxPublishMethod.setItems(httpMethods);
+//
+//            List<UrlMappingReadDto> all = managmentClient.findAll();
+//            ObservableList<UrlMappingReadDto> value = FXCollections.observableArrayList(all);
+//            tableViewUrlMapping.setItems(value);
+//
+//            tableColumnUrlMapping.setCellValueFactory(x -> new SimpleStringProperty(x.getValue().toString()));
+//
+//        } catch (UrlMappingClientException e) {
+//            e.printStackTrace();
+//        }
+
+    }
+
+    private Tab createNoClosableTab(String test) {
+        Tab tabAdd = new Tab(test);
+        tabAdd.setClosable(false);
+        return tabAdd;
+    }
+}
+
+@RequiredArgsConstructor
+class UrlMappingPaneEdit extends BorderPane {
+
+    private final UrlMappingClient managmentClient;
+
+}
+
+@RequiredArgsConstructor
+class UrlMappingPaneAdd extends BorderPane {
+
+    @FXML
+    private Button buttonAdd;
 
     @FXML
     private ComboBox<RequestMethod> comboboxPublishMethod;
@@ -39,32 +110,11 @@ public class UrlMappingPane extends BorderPane {
     @FXML
     private TextField textfieldTargetServer;
 
-    @FXML
-    private TableView<UrlMappingReadDto> tableViewUrlMapping;
-
-    @FXML
-    private TableColumn<UrlMappingReadDto, String> tableColumnUrlMapping;
-
     private final UrlMappingClient managmentClient;
 
 
     @FXML
     private void initialize() {
-
-        try {
-            ObservableList<RequestMethod> httpMethods = FXCollections.observableArrayList(RequestMethod.values());
-            comboboxTargetMethod.setItems(httpMethods);
-            comboboxPublishMethod.setItems(httpMethods);
-
-            List<UrlMappingReadDto> all = managmentClient.findAll();
-            ObservableList<UrlMappingReadDto> value = FXCollections.observableArrayList(all);
-            tableViewUrlMapping.setItems(value);
-
-            tableColumnUrlMapping.setCellValueFactory(x -> new SimpleStringProperty(x.getValue().toString()));
-
-        } catch (UrlMappingClientException e) {
-            e.printStackTrace();
-        }
 
     }
 }
