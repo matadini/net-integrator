d4c7001ca288f47c752b3c7f2a25cc78d85f8601
Mateusz Iwanek <lord.matadini@gmail.com>
2019-10-27 14:49:39 +0100

    elo 2'

--- net-integrator-desktop/src/main/java/pl/inzynier/netintegrator/desktop/gui/script/ScriptPane.java
+++ net-integrator-desktop/src/main/java/pl/inzynier/netintegrator/desktop/gui/script/ScriptPane.java
@@ -5,7 +5,6 @@ import javafx.beans.property.ReadOnlyObjectProperty;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
@@ -13,13 +12,11 @@ import lombok.RequiredArgsConstructor;
 import pl.inzynier.netintegrator.client.mapping.UrlMappingClient;
 import pl.inzynier.netintegrator.client.mapping.dto.UrlMappingReadDto;
 import pl.inzynier.netintegrator.client.script.ScriptClient;
-import pl.inzynier.netintegrator.client.script.dto.ScriptClientException;
-import pl.inzynier.netintegrator.client.script.dto.ScriptReadDto;
 import pl.inzynier.netintegrator.client.script.dto.ScriptType;
-import pl.inzynier.netintegrator.client.script.dto.ScriptWriteDto;
-import pl.inzynier.netintegrator.desktop.Application;
 import pl.inzynier.netintegrator.desktop.shared.ApplicationEvent;
+import pl.inzynier.netintegrator.desktop.shared.JavaFxUtil;
 
+import java.io.IOException;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -29,9 +26,14 @@ public class ScriptPane extends BorderPane {
     @FXML
     private ComboBox<UrlMappingReadDto> comboboxUrlMappings;
 
+    @FXML
+    private TabPane tabPane;
+
     private final UrlMappingClient managmentClient;
 
     private final ScriptClient scriptClient;
+
+    ScriptPaneAdd paneAdd;
 
     @FXML
     public void initialize() {
@@ -42,6 +44,21 @@ public class ScriptPane extends BorderPane {
         SingleSelectionModel<UrlMappingReadDto> selectionModel = comboboxUrlMappings.getSelectionModel();
         ReadOnlyObjectProperty<UrlMappingReadDto> selectedItemProperty = selectionModel.selectedItemProperty();
         selectedItemProperty.addListener(this::onChangedComboboxUrlMapping);
+
+
+        try {
+            paneAdd = new ScriptPaneAdd(managmentClient, scriptClient);
+            paneAdd = JavaFxUtil.loadFxml(paneAdd, ScriptPaneAdd.class.getResource("ScriptPaneAdd.fxml"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Tab add = new Tab("Add");
+        add.setContent(paneAdd);
+
+        tabPane.getTabs().add(add);
+        //  eventBus.register(paneEdit);
+
 
 //        MultipleSelectionModel<ScriptReadDto> selectionModel1 = listViewScripts.getSelectionModel();
 //        ReadOnlyObjectProperty<ScriptReadDto> scriptReadDtoReadOnlyObjectProperty = selectionModel1.selectedItemProperty();
@@ -60,7 +77,7 @@ public class ScriptPane extends BorderPane {
     @Subscribe
     private void handle(ApplicationEvent event) {
 
-        if(ApplicationEvent.URL_MAPPING_CREATE.equals(event) || ApplicationEvent.URL_MAPPING_REMOVE.equals(event)) {
+        if (ApplicationEvent.URL_MAPPING_CREATE.equals(event) || ApplicationEvent.URL_MAPPING_REMOVE.equals(event)) {
             downloadUrlMappings();
         }
 
@@ -91,9 +108,35 @@ public class ScriptPane extends BorderPane {
 }
 
 
+@RequiredArgsConstructor
+class ScriptPaneAdd extends BorderPane {
+
+
+    @FXML
+    private Button buttonNew;
+
+    @FXML
+    private TextArea textFieldScriptContent;
+
+    @FXML
+    private ComboBox<ScriptType> comboboxScriptType;
+
+    private final UrlMappingClient managmentClient;
+
+    private final ScriptClient scriptClient;
+
+    @FXML
+    public void initialize() {
+
+    }
+
+
+}
+
+
 //
 //@RequiredArgsConstructor
-//public class ScriptPane extends BorderPane {
+//public class ScriptPaneAdd extends BorderPane {
 //
 //
 //    @FXML
