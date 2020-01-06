d7b4712f0ca3ade10b6242e3f940e1ef1d85b785
Mateusz Iwanek <lord.matadini@gmail.com>
2020-01-06 17:29:59 +0100

    na uperatego modul uzytkownikow jest git

--- net-integrator-desktop/src/main/java/pl/inzynier/netintegrator/desktop/gui/user/UserPaneRemoveController.java
+++ net-integrator-desktop/src/main/java/pl/inzynier/netintegrator/desktop/gui/user/UserPaneRemoveController.java
@@ -4,8 +4,10 @@ import com.google.common.eventbus.Subscribe;
 import javafx.beans.binding.Bindings;
 import javafx.beans.property.LongProperty;
 import javafx.beans.property.SimpleLongProperty;
+import javafx.beans.property.StringProperty;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -16,6 +18,7 @@ import lombok.RequiredArgsConstructor;
 import pl.inzynier.netintegrator.client.user.UserClient;
 import pl.inzynier.netintegrator.client.user.dto.UserClientException;
 import pl.inzynier.netintegrator.client.user.dto.UserReadDTO;
+import pl.inzynier.netintegrator.desktop.shared.ApplicationSession;
 
 
 import java.util.List;
@@ -32,6 +35,9 @@ class UserPaneRemoveController extends BorderPane {
 
     @FXML
     private Label labelLogin;
+
+    @FXML
+    private Label labelInfo;
 
     @FXML
     private Label labelPassword;
@@ -51,6 +57,27 @@ class UserPaneRemoveController extends BorderPane {
         model = createNewModel();
         download();
         listViewUsers.getSelectionModel().selectedItemProperty().addListener(this::onClickListView);
+        buttonRemove.setOnAction(this::onClickButtonRemove);
+    }
+
+    private void onClickButtonRemove(ActionEvent actionEvent) {
+        try {
+            String login = model.login.get();
+            String loggedUser = ApplicationSession.getInstance().getLoggedUser();
+            if(loggedUser.equals(login)) {
+                labelInfo.setText("Nie można usunąć aktualnie zalogowanego użytkownika");
+            } else {
+                userClient.delete(model.id.get());
+                model = createNewModel();
+                labelInfo.setText("Usunięto użytkownika");
+                download();
+               // la
+            }
+
+
+        } catch (UserClientException e) {
+            e.printStackTrace();
+        }
     }
 
     private void onClickListView(ObservableValue<? extends UserReadDTO> observable, UserReadDTO oldValue, UserReadDTO newValue) {
