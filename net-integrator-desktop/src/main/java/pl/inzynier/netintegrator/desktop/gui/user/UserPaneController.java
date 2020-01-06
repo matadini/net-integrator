89f88fbec7b3be4fef7c7ce118802ef7f7dc15df
Mateusz Iwanek <lord.matadini@gmail.com>
2020-01-06 15:24:46 +0100

    dodwanie userow

--- net-integrator-desktop/src/main/java/pl/inzynier/netintegrator/desktop/gui/user/UserPaneController.java
+++ net-integrator-desktop/src/main/java/pl/inzynier/netintegrator/desktop/gui/user/UserPaneController.java
@@ -1,11 +1,17 @@
 package pl.inzynier.netintegrator.desktop.gui.user;
 
 import com.google.common.eventbus.EventBus;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
 import javafx.scene.layout.BorderPane;
 import lombok.RequiredArgsConstructor;
 import pl.inzynier.netintegrator.client.user.UserClient;
+
+import pl.inzynier.netintegrator.desktop.shared.JavaFxUtil;
+
+import java.net.URL;
 
 
 @RequiredArgsConstructor
@@ -18,9 +24,29 @@ class UserPaneController extends BorderPane {
 
     private final EventBus eventBus;
 
+    private UserPaneAddController userPaneAddController;
+
     @FXML
     public void initialize() {
 
+        try {
+            // dodawanie
+            userPaneAddController = new UserPaneAddController();
+            String path = "UserPaneAdd.fxml";
+            URL resource = UserPaneAddController.class.getResource(path);
+            userPaneAddController = JavaFxUtil.loadFxml(userPaneAddController, resource);
+
+            Tab tabAdd = JavaFxUtil.createNoClosableTab("       Dodawanie       ");
+            tabAdd.setContent(userPaneAddController);
+
+            // dodaj tabsy do widoku
+            ObservableList<Tab> tabs = tabPane.getTabs();
+            tabs.add(tabAdd);
+          //  tabs.add(tabEdit);
+
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
     }
 }
 
